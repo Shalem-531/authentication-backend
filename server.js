def1b4ecff1e585a73c5c5ad7ad988b25330de4a
shalem <shalembadugu@gmail.com>
2026-05-11 15:40:12 +0530

    Setup Backend Server

---
+++ server.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require("cors");
+
+const app = express();
+
+const { sequelize } = require('./config/db');
+const authRoutes = require('./Routes/auth');
+
+// ✅ Middleware
+app.use(express.json());
+
+// ✅ CORS (BEFORE routes)
+app.use(cors({
+  origin: "http://localhost:5173", // if Vite
+  credentials: true
+}));
+
+app.use('/api/users', authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Auth API working ✅");
+});
+
+const PORT = process.env.PORT || 5000;
+
+// Connect DB and start server
+sequelize.sync()
+  .then(() => {
+    console.log("SQLite connected ✅");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT} 🚀`);
+    });
+  })
+  .catch((err) => {
+    console.log("DB Error ❌", err);
+  });
