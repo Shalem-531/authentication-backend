0607afcaf136a854c72970f3496d003ad24cf295
shalem <shalembadugu@gmail.com>
2026-05-13 07:56:45 +0530

    Updated SQLite configuration

--- config/db.js
+++ config/db.js
@@ -1,9 +1,12 @@
 const path = require('path');
 const { Sequelize } = require('sequelize');
+const sqlJsAsSqlite3 = require('sql.js-as-sqlite3');
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: path.join(__dirname, '../database.sqlite')
+  dialectModule: sqlJsAsSqlite3,
+  storage: path.join(__dirname, '../database.sqlite'),
+  logging: false
 });
 
 module.exports = { sequelize };
