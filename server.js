3513a88b7fee4b305c429121990bde64965e7c5e
shalem <shalembadugu@gmail.com>
2026-05-15 11:35:56 +0530

    Removed / in vercel link

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // ✅ CORS (BEFORE routes)
 app.use(cors({
-  origin: "https://e-commmerce-frontend-theta.vercel.app/", 
+  origin: "https://e-commmerce-frontend-theta.vercel.app", 
   credentials: true
 }));
 
